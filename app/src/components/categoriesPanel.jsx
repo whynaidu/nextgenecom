1fb5c5828d52ecbcce28906c4783157b71d3b27a
whynaidu <naiduvedant@gmail.com>
2023-03-27 20:31:58 +0530

    STARTED MAKING SOME UI ELEMENTS

---
+++ app/src/components/categoriesPanel.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function CategoriesPanel() {
+  return (
+    <div className="p-3 grid grid-cols-8 grid-flow-col gap-3 place-items-center">
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+      <div className="bg-white rounded-full h-24 w-24 flex justify-center items-center text-black">
+        lkmsf
+      </div>
+    </div>
+  );
+}
---
+++ app/src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='bg-white text-black w-full rounded-full h-24 p-6'>
+      dsfdf
+    </div>
+  )
+}
